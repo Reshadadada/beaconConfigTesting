819cb5d2c152fb3cfe77b1632a2ab6bf7ebc9d24
reshad <a.hamauon@wustl.edu>
2021-02-02 13:52:50 -0600

    Update completeConfigTests.py

--- completeConfigTests.py
+++ completeConfigTests.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         for direc in direction_list:
             direction = direc
             scenario = obstacle + " " +  distance + " " +  transmission_power + " " +  direction
-            input("Press Enter then face " + str(direction)")
+            input("Press Enter then face " + str(direction))
             scanner = BeaconScanner(callback,
                 # remove the following line to see packets from all beacons
                 device_filter = IBeaconFilter(minor = beacon_minor), 
